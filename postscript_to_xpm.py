cabe597eaa19b050c686fc47495d9739b888a715
linkyndy <linkyndy@gmail.com>
2014-03-26 23:02:22 +0200

    Added support for line clipping in Postscript to XPM script

--- postscript_to_xpm.py
+++ postscript_to_xpm.py
@@ -9,6 +9,18 @@ parser.add_argument(
 parser.add_argument('-w', '--width', type=int, help='Width of XPM image')
 parser.add_argument('-h', '--height', type=int, help='Height of XPM image')
 parser.add_argument('-o', '--output', help='Output file (of XPM type)')
+parser.add_argument(
+    '-wl', '--window-left', type=int,
+                            help='Left limit of clipping rectangle')
+parser.add_argument(
+    '-wt', '--window-top', type=int,
+                           help='Top limit of clipping rectangle')
+parser.add_argument(
+    '-wr', '--window-right', type=int,
+                             help='Right limit of clipping rectangle')
+parser.add_argument(
+    '-wb', '--window-bottom', type=int,
+                              help='Bottom limit of clipping rectangle')
 args = parser.parse_args()
 
 # Keep only valid lines from postscript file (the ones ending with LINE),
@@ -24,7 +36,19 @@ for line in lines:
     line[0], line[1] = args.height - line[1], line[0]
     line[2], line[3] = args.height - line[3], line[2]
 
+# Check whether lines need clipping
+clipping = (args.window_left and args.window_top and
+            args.window_right and args.window_bottom)
+
 image = XPM(args.width, args.height)
-for line in lines:
-    image.line(*line, color=Color('#FF0000', 'R'))
+if clipping:
+    for line in lines:
+        image.clipped_line(*line, xmin=args.window_left,
+                                  ymin=args.window_bottom,
+                                  xmax=args.window_right,
+                                  ymax=args.window_top,
+                                  color=Color('#FF0000', 'R'))
+else:
+    for line in lines:
+        image.line(*line, color=Color('#FF0000', 'R'))
 image.export(args.output, autofill=True)
