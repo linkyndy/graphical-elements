564692a27acfc89e492a570d77bd0221582f96df
linkyndy <linkyndy@gmail.com>
2014-04-01 20:38:45 +0300

    Changed clipped_line.py to reflect library changes

--- clipped_line.py
+++ clipped_line.py
@@ -2,5 +2,5 @@ from xpm import XPM, Color
 
 
 image = XPM(10, 10)
-image.clipped_line(0, 0, 9, 9, 4, 4, 6, 6, Color('#FF0000', 'R'))
+image.complex_line(0, 0, 9, 9, clip=(4, 4, 6, 6), color=Color('#FF0000', 'R'))
 image.export('clipped_line.xpm', autofill=True)
