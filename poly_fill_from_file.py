9425b81d4e7c6197ea8380208bad04d130bb4765
linkyndy <linkyndy@gmail.com>
2014-05-15 21:20:01 +0300

    Added more options for filling a poly from file

--- poly_fill_from_file.py
+++ poly_fill_from_file.py
@@ -25,6 +25,30 @@ parser.add_argument(
 parser.add_argument(
     '-b', '--blue',
     type=int, help='Blue component of fill color')
+parser.add_argument(
+    '-wl', '--window-left',
+    type=int, help='Left limit of clipping rectangle')
+parser.add_argument(
+    '-wt', '--window-top',
+    type=int, help='Top limit of clipping rectangle')
+parser.add_argument(
+    '-wr', '--window-right',
+    type=int, help='Right limit of clipping rectangle')
+parser.add_argument(
+    '-wb', '--window-bottom',
+    type=int, help='Bottom limit of clipping rectangle')
+parser.add_argument(
+    '-vl', '--viewport-left',
+    type=int, default=0, help='Left limit of viewport rectangle')
+parser.add_argument(
+    '-vt', '--viewport-top',
+    type=int, default=0, help='Top limit of viewport rectangle')
+parser.add_argument(
+    '-vr', '--viewport-right',
+    type=int, help='Right limit of viewport rectangle')
+parser.add_argument(
+    '-vb', '--viewport-bottom',
+    type=int, help='Bottom limit of viewport rectangle')
 args = parser.parse_args()
 
 # Vertices are stored one per line in a .pol file, with x and y coords
@@ -35,9 +59,24 @@ vertices = [map(int, line.replace('\n', '').split())
 # Compute HEX value for given RGB color
 color = Color('#%02x%02x%02x' % (args.red, args.green, args.blue), 'C')
 
+# Check whether poly needs clipping
+clipping = (args.window_left and args.window_top and
+            args.window_right and args.window_bottom)
+
+# Set defaults for remaining viewport args
+args.viewport_right = args.viewport_right or args.width-1
+args.viewport_bottom = args.viewport_bottom or args.height-1
+
+# Compute viewport scale factors
+x_factor = 1.*(args.viewport_bottom-args.viewport_top)/(args.window_bottom-args.window_top)
+y_factor = 1.*(args.viewport_right-args.viewport_left)/(args.window_right-args.window_left)
+
 image = XPM(args.width, args.height)
-image.poly(
+image.scale(args.viewport_top, args.viewport_left, x_factor, y_factor)
+image.complex_poly(
     vertices=vertices,
     color=color,
-    fill=color)
+    fill=color,
+    clip=(args.window_left, args.window_bottom,
+          args.window_right, args.window_top) if clipping else None)
 image.export(args.output, autofill=True)
